ed783121fc1aac250e54299440fa70e9a4f173cb
rasel739 <raselhossaintro@gmail.com>
2022-06-16 15:03:59 +0600

    add all file

---
+++ src/components/javascript.js
@@ -0,0 +1,40 @@
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let reuslt = array.reduce((prev, curr) => {
+  return prev + curr;
+}, 0);
+
+console.log(reuslt);
+
+let array2 = [
+  {
+    name: "Mobile",
+    price: 50,
+  },
+  {
+    name: "leptop",
+    price: 50,
+  },
+  {
+    name: "watch",
+    price: 50,
+  },
+  {
+    name: "watch",
+    price: 50,
+  },
+  {
+    name: "watch",
+    price: 50,
+  },
+  {
+    name: "watch",
+    price: 50,
+  },
+];
+
+let result2 = array2.reduce((prev, curr) => {
+  return prev + curr.price;
+}, 0);
+
+console.log(result2);
